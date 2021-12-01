06a38521356daad50a69284ab53f74f83fe971c4
prlakshmisri <lakshmisri.pr20@gmail.com>
2021-12-01 14:17:21 +0530

    employee details commit

---
+++ D1-EmployeeDetails/src/org/emp/Employee.java
@@ -0,0 +1,54 @@
+package org.emp;
+
+public class Employee {
+
+	public void empId() {
+		// TODO Auto-generated method stub
+
+		System.out.println("Employee ID is 01");
+	}
+	
+	public void empName() {
+		// TODO Auto-generated method stub
+		System.out.println("Employee Name is Lakshmi");
+
+	}
+	
+	public void empDob() {
+		// TODO Auto-generated method stub
+		System.out.println("Employee DOB is 01/10/1995");
+
+	}
+	
+	public void empPhone() {
+		// TODO Auto-generated method stub
+		System.out.println("Employee phone is 9234567889");
+
+	}
+	
+	public void empEmail() {
+		// TODO Auto-generated method stub
+		System.out.println("Employee email is lakshmi@gmail.com");
+
+	}
+	
+	public void empAddress() {
+		// TODO Auto-generated method stub
+		System.out.println("Employee address is Annanagar");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee e=new Employee();
+		e.empId();
+		e.empName();
+		e.empDob();
+		e.empPhone();
+		e.empEmail();
+		e.empAddress();
+		
+	}
+	
+	
+}
